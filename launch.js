87fba058dcf3c89cda021cca393e0bebfd2fd2a2
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2020-06-23 15:08:02 +0200

    Filecheck for existing usernames

--- launch.js
+++ launch.js
@@ -1,5 +1,6 @@
 const App = require("./src/index");
 var secret = require("./secret");
+const fs = require('fs')
 const Telebot = require('telebot');
 const bot = new Telebot({
 	token: secret.bottoken,
@@ -13,8 +14,14 @@ bot.on(/^\/run(.+)$/i, (msg, props) => {
     var Para = props.match[1].split(' ');
     var Username = Para[1].toLowerCase();
     console.log(Username)
-    App.main(Username).then(function(Output) {
-	msg.reply.text(`Made by: https://github.com/duiker101/twitter-interaction-circles\nTelegram version made by @BolverBlitz`);
+    if (fs.existsSync('/var/www/html/' + Username + '.png')) {
+        console.log("Exist")
+        msg.reply.text(`Made by: https://github.com/duiker101/twitter-interaction-circles\nTelegram version made by @BolverBlitz`);
         bot.sendPhoto(msg.chat.id, "http://v2.bolverblitz.net/" + Username + ".png")
-    }).catch(error => msg.reply.text(`ImgGen Error: ${error.errors[0].message}`));
+    }else{
+        App.main(Username).then(function(Output) {
+            msg.reply.text(`Made by: https://github.com/duiker101/twitter-interaction-circles\nTelegram version made by @BolverBlitz`);
+            bot.sendPhoto(msg.chat.id, "http://v2.bolverblitz.net/" + Username + ".png")
+        }).catch(error => msg.reply.text(`ImgGen Error: ${error.errors[0].message}`));
+      }
 });
