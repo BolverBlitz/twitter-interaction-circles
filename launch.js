5cc459951966395116f4f57ef01c0cee7ed846f4
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2020-07-11 21:34:59 +0200

    Update launch.js

--- launch.js
+++ launch.js
@@ -8,7 +8,13 @@ const bot = new Telebot({
         usePlugins: ['commandButton']
 });
 
+const YourWebserver = "https://bolverblitz.net/" //the / at the end is important!
+
 bot.start(); //Telegram bot start
+
+bot.on(/^\/start$/i, (msg) => {
+	msg.reply.text(`Type /run username without the @, the bot then will start working on your request this can take up to 30s`)
+});
 
 bot.on(/^\/run(.+)$/i, (msg, props) => {
     var Para = props.match[1].split(' ');
@@ -16,13 +22,13 @@ bot.on(/^\/run(.+)$/i, (msg, props) => {
     console.log(Username)
     if (fs.existsSync('/var/www/html/' + Username + '.png')) {
         console.log("Exist")
-        bot.sendPhoto(msg.chat.id, "http://v2.bolverblitz.net/" + Username + ".png").then(function(Output) {
-            msg.reply.text(`Made by: https://github.com/duiker101/twitter-interaction-circles\nTelegram version made by @BolverBlitz\nDownload full png: http://v2.bolverblitz.net/${Username}.png`);
+        bot.sendPhoto(msg.chat.id, YourWebserver + Username + ".png").then(function(Output) {
+            msg.reply.text(`Made by: https://github.com/duiker101/twitter-interaction-circles\nTelegram version made by @BolverBlitz\nDownload full png: ${YourWebserver}/${Username}.png`);
         });
     }else{
         App.main(Username).then(function(Output) {
-            bot.sendPhoto(msg.chat.id, "http://v2.bolverblitz.net/" + Username + ".png").then(function(Output) {
-                msg.reply.text(`Made by: https://github.com/duiker101/twitter-interaction-circles\nTelegram version made by @BolverBlitz\nDownload full png: http://v2.bolverblitz.net/${Username}.png`);
+            bot.sendPhoto(msg.chat.id, YourWebserver + Username + ".png").then(function(Output) {
+                msg.reply.text(`Made by: https://github.com/duiker101/twitter-interaction-circles\nTelegram version made by @BolverBlitz\nDownload full png: ${YourWebserver}/${Username}.png`);
             });
         }).catch(error => msg.reply.text(`ImgGen Error: ${error.errors[0].message}`));
       }
